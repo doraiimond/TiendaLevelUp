b9a612bb84519a9c9753800466516b922dedc4f9
The_MisterG <robi.contreras@duocuc.cl>
2025-10-07 18:30:43 -0300

    Update PerfilAdmin.jsx

---
+++ src/components/pages/PerfilAdmin.jsx
@@ -0,0 +1,8 @@
+import { useLocation } from "react-router-dom";
+
+function useQuery() { return new URLSearchParams(useLocation().search);}
+const PerfilAdmin = () => {
+    const q = useQuery();
+    return <h2>Perfil Admin - Bienvenido { q.get("nombre")}</h2>
+};
+export default PerfilAdmin;
