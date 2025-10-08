20b479b691ce4c39ed852dc8258a44f9a06535a1
The_MisterG <robi.contreras@duocuc.cl>
2025-10-08 16:36:04 -0300

    correccion error firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA-8bvs0ogm0HAqr-ngrC-K21Lxg4WRavI",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const db = getFirestore(app);
