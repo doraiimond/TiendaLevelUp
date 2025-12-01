575352d3791eb125bbc267411ea0ae6004835443
doraiimond <carl.acevedom@duocuc.cl>
2025-11-30 21:02:14 -0300

    Login fix

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -27,9 +27,9 @@ class DashboardManager {
             
             const firebaseConfig = {
                 apiKey: "AIzaSyA-8bvs0ogm0HAqr-ngrC-K21Lxg4WRavI",
-                authDomain: "tiendalevelup-66aa8.firebaseapp.com",
-                projectId: "tiendalevelup-66aa8",
-                storageBucket: "tiendalevelup-66aa8.appspot.com", //actualizar
+                authDomain: "levelup-7269f.firebaseapp.com",
+                projectId: "levelup-7269f",
+                storageBucket: "levelup-7269f.appspot.com", //actualizar
                 messagingSenderId: "500431750193",
                 appId: "1:500431750193:web:b4542d0c40934a02034110",
                 measurementId: "G-87KTP9XR6E"
